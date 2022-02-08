b1c21da7d2aa2b444b9aa05cf92ed6df7b55825f
Hiradoras <forwoermind@gmail.com>
2022-02-08 17:55:42 +0600

    Added addToInventory function

---
+++ fantesy_game_inventory.py
@@ -0,0 +1,32 @@
+import string
+
+
+inventory = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+
+def displayInventory(inventory : dict) -> string:
+    print("Inventory:")
+    for k,v in inventory.items():
+        print('%i : %s\n' % (v, k))
+
+print('Original Inventory: ')
+displayInventory(inventory)
+
+
+
+def addToInventory(inventory, added_items):
+    set_added_items = set(added_items)
+    items = list(inventory.keys())
+    for i in set_added_items:
+        if i not in items:
+            inventory[i] = added_items.count(i)
+        else:
+            inventory[i]+= added_items.count(i)
+            
+
+
+dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+
+addToInventory(inventory, dragon_loot)
+
+print("New inventory")
+displayInventory(inventory)
